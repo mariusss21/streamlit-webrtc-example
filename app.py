04ef61c7fa4dde6e2d719f1d32907f83da5ea3e6
Mario Carvalho <39649854+mariusss21@users.noreply.github.com>
2022-03-18 13:12:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -75,19 +75,23 @@ st.set_page_config(
 
 
 def read_barcodes(frame):
-    barcodes = decode(frame)
-    for barcode in barcodes:
-        x, y , w, h = barcode.rect        #1
-        barcode_info = barcode.data.decode('utf-8')
-        # cv2.rectangle(frame, (x, y),(x+w, y+h), (0, 255, 0), 2)
-        
-        # #2
-        # font = cv2.FONT_HERSHEY_DUPLEX
-        # cv2.putText(frame, barcode_info, (x + 6, y - 6), font, 2.0, (255, 255, 255), 1)        #3
-        # with open("barcode_result.txt", mode ='w') as file:
-        #     file.write("Recognized Barcode:" + barcode_info)    
+    try:
+        barcodes = decode(frame)
+	
+        for barcode in barcodes:
+            x, y , w, h = barcode.rect        #1
+            barcode_info = barcode.data.decode('utf-8')
+            # cv2.rectangle(frame, (x, y),(x+w, y+h), (0, 255, 0), 2)
             
-        return barcode_info
+            # #2
+            # font = cv2.FONT_HERSHEY_DUPLEX
+            # cv2.putText(frame, barcode_info, (x + 6, y - 6), font, 2.0, (255, 255, 255), 1)        #3
+            # with open("barcode_result.txt", mode ='w') as file:
+            #     file.write("Recognized Barcode:" + barcode_info)    
+                
+            return barcode_info
+    except:
+        return None
 
 
 # Configurando o acesso ao mongodb
