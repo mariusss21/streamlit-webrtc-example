dc71ab06baf5f81d68b4aefc9d1aa4474c319c07
Mario Carvalho <mariocarvalho@pop-os.localdomain>
2022-03-31 13:03:26 -0300

    corrigindo falhas do commit anterior

--- app.py
+++ app.py
@@ -87,67 +87,55 @@ def read_barcodes(frame):
 
 def entrada_bobinas() -> None:
     st.subheader('Inserir bobina')
-    status_bobina = st.selectbox('Status da bobina', ['Liberado', 'Não conforme']) # data
 
     with st.form(key='myform'):
         texto_qrcode = ''
 
-        if status_bobina == 'Não conforme':
-            produto_bobina = st.text_input('Produto:')
-            quantidade_bobina = st.number_input('Quantidade:', format='%i', step=1, value=9000)
-            lote_bobina = st.text_input('Lote:')
-            fornecedor_bobina = st.text_input('Fornecedor:')
-            data_bobina = st.date_input('Data entrada:')
+        dict_tipo_bobinas = {
+        'BOBINA ALUMINIO LATA 16 OZ COIL 00098': 50761710,
+        'BOBINA ALUMINIO LATA 12 OZ COIL 00098': 50679811,
+        'BOBINA ALUMINIO LATA 12 OZ COIL 98 SCRAP': 40011008,
+        'BOBINA ALUMINIO LACRE PRETO': 50552903,
+        'BOBINA ALUMINIO LACRE AZUL': 50527602,
+        'BOBINA ALUMINIO LATA 16 OZ': 50490762,
+        'BOBINA ALUMINIO LATA 12 OZ': 50490761,
+        'BOBINA ALUMINIO TAMPA PRATA REFRIG.': 50490760,
+        'BOBINA ALUMINIO TAMPA DOURADO CERVEJA': 50490599,
+        'BOBINA ALUMINIO LACRE PRATA': 50490598,
+        'BOBINA ALUMINIO LATA 12 OZ SCRAP': 40010824,
+        'BOBINA ALUMINIO TAMPA BRANCA': 50527252,
+        'BOBINA ALUMINIO LACRE DOURADO': 50771048}
 
-            texto_qrcode = ''.join(('Produto: ',
-                produto_bobina,
-                '; Quantidade: ',
-                str(quantidade_bobina),
-                '; Lote: ',
-                lote_bobina,
-                '; Fornecedor: ',
-                fornecedor_bobina,
-                '; Data entrada: ',
-                str(data_bobina))                    
-            )
-
-        if status_bobina == 'Liberado':
-            descricao_bobina = st.text_input('Descrição:')
-            conferente_bobina = st.text_input('Conferente:')
-            quantidade_bobina = st.number_input('Quantidade:', format='%i', step=1, value=9000)
-            lote_bobina = st.text_input('Lote SAP:')
-
-            dict_tipo_bobinas = {
-            'BOBINA ALUMINIO LATA 16 OZ COIL 00098': 50761710,
-            'BOBINA ALUMINIO LATA 12 OZ COIL 00098': 50679811,
-            'BOBINA ALUMINIO LATA 12 OZ COIL 98 SCRAP': 40011008,
-            'BOBINA ALUMINIO LACRE PRETO': 50552903,
-            'BOBINA ALUMINIO LACRE AZUL': 50527602,
-            'BOBINA ALUMINIO LATA 16 OZ': 50490762,
-            'BOBINA ALUMINIO LATA 12 OZ': 50490761,
-            'BOBINA ALUMINIO TAMPA PRATA REFRIG.': 50490760,
-            'BOBINA ALUMINIO TAMPA DOURADO CERVEJA': 50490599,
-            'BOBINA ALUMINIO LACRE PRATA': 50490598,
-            'BOBINA ALUMINIO LATA 12 OZ SCRAP': 40010824,
-            'BOBINA ALUMINIO TAMPA BRANCA': 50527252,
-            'BOBINA ALUMINIO LACRE DOURADO': 50771048}
-
-            tipo_bobina = st.selectbox('Tipo', dict_tipo_bobinas.keys)
-            data_bobina = st.date_input('Data entrada:')
-
-            texto_qrcode = ''.join(('Código SAP: ',sap_bobina,
-                '; Descrição: ',descricao_bobina,
-                '; Conferente:',conferente_bobina,
-                '; Quantidade: ',str(quantidade_bobina),
-                '; Lote: ',lote_bobina,
-                '; Tipo:',tipo_bobina,
-                '; Data entrada: ',str(data_bobina))                    
-            )
+        status_bobina = st.selectbox('Status da bobina', ['Liberado', 'Não conforme']) # data
+        descricao_bobina = st.text_input('Descrição:')
+        conferente_bobina = st.text_input('Conferente:')
+        quantidade_bobina = st.number_input('Quantidade:', format='%i', step=1, value=9000)
+        lote_bobina = st.text_input('Lote SAP:')
+        tipo_bobina = st.selectbox('Tipo', dict_tipo_bobinas.keys)
+        data_bobina = st.date_input('Data entrada:')
 
         submit_button = st.form_submit_button(label='Salvar bobina')
 
         if submit_button:
+            tipo_de_etiqueta = ''
+            
+            if status_bobina == 'Não conforme':
+                tipo_de_etiqueta = 'BLOQUEADO'
+
+            if status_bobina == 'Liberado':
+                tipo_de_etiqueta = 'LIBERADO'
+
             sap_bobina = dict_tipo_bobinas[tipo_bobina]
+            
+            texto_qrcode = ''.join(('tipo de etiqueta: ', tipo_de_etiqueta,
+                ';Código SAP: ', sap_bobina,
+                '; Descrição: ', descricao_bobina,
+                '; Conferente:', conferente_bobina,
+                '; Quantidade: ', str(quantidade_bobina),
+                '; Lote: ', lote_bobina,
+                '; Tipo:', tipo_bobina,
+                '; Data entrada: ',str(data_bobina))                    
+            )
 
             st.subheader('Informação do qrcode')
             st.write(texto_qrcode)
