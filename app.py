e65feeed8813b141864478d29eb7debd6072efaa
Mario Carvalho <mariocarvalho@pop-os.localdomain>
2022-04-01 11:51:25 -0300

    filtro de data para etiquetas e filtro de status

--- app.py
+++ app.py
@@ -204,7 +204,7 @@ def download_etiqueta(texto_qrcode: str, dados_bobina: pd.DataFrame) -> None:
         ws = wb['LIBERADO']
 
         st.write(ws['A9'].value)
-        ws['A23'] = imagem_bobina_qr
+        ws['A23'] = imagem_bobina_qr.getvalue()
 
 
     if dados_bobina.loc['tipo_de_etiqueta'] == 'BLOQUEADO':
