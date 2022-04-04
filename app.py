dc9229287b30dfa7ec6bd1df26fea070b13c0e4b
Mario Carvalho <mariocarvalho@pop-os.localdomain>
2022-04-04 13:51:14 -0300

    inserindo menu do inventário

--- app.py
+++ app.py
@@ -242,8 +242,8 @@ def download_etiqueta(texto_qrcode: str, dados_bobina: pd.DataFrame) -> None:
         st.write(dados_bobina.astype(str))
 
         ws['A2'] = dados_bobina.loc['sap'] #codigo do produto
-        ws['A5'] = dados_bobina.loc['quantidade'] #quantidade do produto
-        ws['A9'] = dados_bobina.loc['lote'] #lote do produto
+        ws['A3'] = dados_bobina.loc['quantidade'] #quantidade do produto
+        ws['A5'] = dados_bobina.loc['lote'] #lote do produto
         ws['A13'] = dados_bobina.loc['data'] #data de entrada do produto
 
     wb.save('Etiqueta_download.xlsx')
