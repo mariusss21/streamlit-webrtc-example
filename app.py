8b9839d5505224e669092ddae8c678c3bf000ed0
Mario Carvalho <mariocarvalho@pop-os.localdomain>
2022-04-05 12:13:34 -0300

    removendo login

--- app.py
+++ app.py
@@ -247,13 +247,15 @@ def VideoProcessor():
             data, points, _ = decoder.detectAndDecode(img)
             
             if points is not None:
-                print('Decoded data: ' + data)
+                # print('Decoded data: ' + data)
             
                 points = points[0]
                 for i in range(len(points)):
                     pt1 = [int(val) for val in points[i]]
                     pt2 = [int(val) for val in points[(i + 1) % 4]]
                     cv2.line(img, pt1, pt2, color=(255, 0, 0), thickness=3)
+                    cv2.putText(img=img, text=data, org=(150, 250), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=3, color=(255, 0, 0),thickness=3)
+
 
             return av.VideoFrame.from_ndarray(img, format='bgr24')
 
