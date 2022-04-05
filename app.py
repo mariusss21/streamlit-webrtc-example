208411586b3589ee2d9a610c34d02d5b971fa5b3
Mario Carvalho <mariocarvalho@pop-os.localdomain>
2022-04-05 09:56:15 -0300

    removendo login

--- app.py
+++ app.py
@@ -196,8 +196,12 @@ def inserir_invetario() -> None:
 
     if video_frame is not None:
         st.write(type(video_frame))
-        file_bytes = io.BytesIO(video_frame )#.getvalue())
-        image = cv2.imdecode(np.frombuffer(file_bytes.read(), np.uint8), cv2.IMREAD_COLOR)
+        # file_bytes = io.BytesIO(video_frame )#.getvalue())
+        byte_io = io.BytesIO()
+        # video_frame.save(byte_io, format='PNG')
+
+        image = cv2.imdecode(np.frombuffer(video_frame.save(byte_io, format='PNG'), np.uint8), cv2.IMREAD_COLOR)
+        # image = cv2.imdecode(np.frombuffer(file_bytes.read(), np.uint8), cv2.IMREAD_COLOR)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         blur = cv2.medianBlur(gray, 5)
         valor = read_barcodes(blur)
