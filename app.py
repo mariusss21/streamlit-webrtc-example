0549724ea1f2f1649fa561a66d85d73fcc514fb0
Mario Carvalho <mariocarvalho@pop-os.localdomain>
2022-04-07 14:42:28 -0300

    baixando a versão do streamlit

--- app.py
+++ app.py
@@ -250,7 +250,7 @@ def VideoProcessor(dataframe_string: str) -> None:
             file_bytes = io.BytesIO(buf.getvalue())
             imagem = cv2.imdecode(np.frombuffer(file_bytes.read(), np.uint8), cv2.IMREAD_COLOR)
             blur = cv2.medianBlur(imagem, 5)
-            data = read_barcodes(imagem)
+            data = read_barcodes(frame)
 
 
             if data != '' and data is not None:
